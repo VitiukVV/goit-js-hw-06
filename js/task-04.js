6b1d544f2322327ad45e2018b1806d877c874cd5
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-06-29 22:06:00 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+let counterValue = 0;
+const value = document.querySelector("#value");
+const decrement = document.querySelector('[data-action="decrement"]');
+const increment = document.querySelector('[data-action="increment"]');
+
+const handlClickDecrement = () => {
+  counterValue -= 1;
+  return (value.textContent = counterValue);
+};
+
+const handlClickIncrement = () => {
+  counterValue += 1;
+  return (value.textContent = counterValue);
+};
+// function onClick(event) {
+//   if (event === decrement) {
+//     counterValue -= 1;
+//   }
+//   if (event === increment) {
+//     counterValue += 1;
+//   }
+//   return (value.textContent = counterValue);
+// }
+decrement.addEventListener("click", handlClickDecrement);
+increment.addEventListener("click", handlClickIncrement);
