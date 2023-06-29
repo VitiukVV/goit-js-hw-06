adacd00507dcac1db6880c826eb8f0dc7b1e9d9d
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-06-29 19:41:25 +0300

    task 02 and 03

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientsArr = ingredients.map((ingredient) => {
+  const liItem = document.createElement("li");
+  liItem.classList.add("item");
+  liItem.textContent = ingredient;
+  return liItem;
+});
+list.append(...ingredientsArr);
