444496c5f95dacad314cd12ed8b39c3ecaa45d0c
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-07-02 12:35:52 +0300

    task 09 and 10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const nameColor = document.querySelector(".color");
+const body = document.body;
+
+changeColorBtn.addEventListener("click", onClick);
+// варінт коли в стилі і в значенні показує однаковий колір rgb
+function onClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  nameColor.textContent = body.style.backgroundColor;
+}
+
+// варіант коли в стилі колір rgb а в значенні hex
+// function onClick() {
+//   const randomColor = getRandomHexColor();
+//   body.style.backgroundColor = randomColor;
+//   nameColor.textContent = randomColor;
+// }
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.querySelector("#controls");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input[type='number']");
+
+createBtn.addEventListener("click", function () {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let sizeEl = 30;
+  let newDiv = "";
+  for (let i = 0; i < amount; i += 1) {
+    const sizeDiv = sizeEl + i * 10;
+    const color = getRandomHexColor();
+    newDiv += `<div style="background-color: ${color}; width: ${sizeDiv}px; height: ${sizeDiv}px;"></div>`;
+  }
+  boxes.innerHTML = newDiv;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
