70d2cad9c050a41affe3a658376005e005ac8971
Volodymyr Vitiuk <v.v.endeavour@gmail.com>
2023-07-02 08:44:13 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("The fields must be completed!");
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  }
+  event.currentTarget.reset();
+}
